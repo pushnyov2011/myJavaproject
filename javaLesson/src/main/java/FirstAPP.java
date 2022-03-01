dd3eecbaaac3a939471b0c8e38f9f64a80214e44
ssergey <pushnyov2011@ya.ru>
2022-03-01 22:38:33 +0300

    First commit

---
+++ javaLesson/src/main/java/FirstAPP.java
@@ -0,0 +1,10 @@
+public class FirstAPP {
+    // программа из лекции
+    public static void main(String[] args)
+    {
+        System.out.println("Hellow, world");
+        System.out.println();
+        System.out.println(4);
+    }
+
+}
