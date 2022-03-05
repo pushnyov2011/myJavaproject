8373a06474fcdcd65a68bd23f2f86bb2a847e258
sergey <pushnyov2011@ya.ru>
2022-03-05 21:17:14 +0300

    Домашние задание 2 задание 1

---
+++ javaLesson/src/main/java/HomeWork2/Zadania1.java
@@ -0,0 +1,38 @@
+package HomeWork2;
+import java.util.Scanner;
+
+public class Zadania1 {
+    public static  void main (String[] args)
+        {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("введеди целое число а");
+            int a = sc.nextInt();
+            System.out.println("введеди целое число b ");
+            int b = sc.nextInt();
+            sc.close();
+            System.out.println(metod(a,b));
+
+
+        }
+
+        public static boolean metod (int a ,int b)
+        {
+            int c = a+b;
+            if (c > 10 &&  c <=20 )
+            {
+                return true;
+            }
+            else return false;
+
+
+        }
+        /*
+        Написать метод, принимающий на вход два целых числа и проверяющий, что их
+сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в
+противном случае – false.
+
+         */
+
+
+}
