236eeec7f0cf32f1ac1535bdb65cbfeba03b741d
sergey <pushnyov2011@ya.ru>
2022-03-05 21:31:29 +0300

    Домашние задание 2 задание 2

---
+++ javaLesson/src/main/java/HomeWork2/Zadania2.java
@@ -0,0 +1,31 @@
+package HomeWork2;
+import java.util.Scanner;
+
+public class Zadania2 {
+    public static void  main (String[] args)
+    {
+        /*
+        Написать метод, которому в качестве параметра передается целое число, метод
+должен напечатать в консоль, положительное ли число передали или
+отрицательное. Замечание: ноль считаем положительным числом.
+
+         */
+        System.out.println("Введите целое цисло =");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println(metod(a));
+
+
+
+    }
+
+    public  static  String  metod(int a)
+    {
+        if (a >= 0)
+        {
+            return  "Число положительное";
+        }
+        else return  "Число не положительное, а отрицательное ";
+    }
+}
