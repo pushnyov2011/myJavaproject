65cc990abbf16e964a3ac30b7c8154b36d113150
sergey <pushnyov2011@ya.ru>
2022-03-05 22:58:12 +0300

    Домашние задание 2 задание 4

---
+++ javaLesson/src/main/java/HomeWork2/Zadania4.java
@@ -0,0 +1,33 @@
+package HomeWork2;
+import  java.util.Scanner;
+
+public class Zadania4 {
+    public static void main (String[] args)
+    {
+        /*
+        Написать метод, которому в качестве аргументов передается строка и число, метод
+должен отпечатать в консоль указанную строку, указанное количество раз;
+
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите строку");
+        String st = sc.nextLine();
+       // System.out.println(st);
+        System.out.println("Сколько рвз вывести эту строку ? введите число");
+        int a = sc.nextInt();
+       cicle(st,a);
+
+
+
+    }
+    public static void  cicle (String a, int b)
+    {
+        if (b>0){
+        for (int i = 0; i < b ; i++)
+            System.out.println(a);}
+        else System.out.println("так число бяло меньше или равер 0, то нчегт не выведу ");
+
+    }
+
+    }
+
---
+++ javaLesson/src/main/java/HomeWork2/Zv1.java
@@ -0,0 +1,11 @@
+package HomeWork2;
+
+public class Zv1 {
+    public static void  main (String[] args)
+    {
+        /*
+
+         */
+
+    }
+}
